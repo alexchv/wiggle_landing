5ff30cab77b6018672f7eac3650a7f706808ac3e
Aleksandr Cherednikov <aleksandr.cherednikov.tgn@gmail.com>
2015-07-07 11:59:44 +0300

    landing feature block refactoring

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -4,4 +4,10 @@ module ViewHelper
     content_for(:page_title) { title.to_s }
   end
 
+  def feature_landing(klasses, name, description)
+    content_tag(:div, :class => 'col-md-4') do
+      safe_join([content_tag(:div, '', :class => klasses), content_tag(:h2, name), content_tag(:p, description)])
+    end
+  end
+
 end
